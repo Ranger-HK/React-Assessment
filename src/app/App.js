c63e22b362cbdc2c1075c917157b83c7081234dd
Ranger-HK <maxravindu99@gmail.com>
2022-08-09 19:50:12 +0530

    Dashboard complete

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,34 @@
-import React from "react";
-import Login from "../pages/session/login";
+// import React from "react";
+// import Login from "../pages/session/login";
+//
+// function App() {
+//     return (
+//       <Login></Login>
+//
+//
+//     );
+// }
+//
+// export default App;
+
+import NavBar from "../components/common/NavBar";
+
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Product from "../pages/Product";
 
 function App() {
-    return (
-      <Login></Login>
+    return (<>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/product" element={<Product/>}/>
+                <Route path="/cart" element={<Dashboard/>}/>
+
+            </Routes>
+        </>
+
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {StyledEngineProvider} from "@mui/material/styles";
+import reportWebVitals from "./reportWebVitals";
+import App from "./app/App";
+import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </StyledEngineProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
